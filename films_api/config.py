5915c425d0606c7df28a5acaa9baedb469272870
GreenFor3st <greenforest249@gmail.com>
2023-05-06 11:02:27 +0100

    Creation or validation and serialization\deserialization of data. Creation of application architecture

--- films_api/config.py
+++ films_api/config.py
@@ -7,3 +7,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "flask_tutorial.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'you-will-never-know'
+    # SQLALCHEMY_ECHO = True
--- films_api/src/__init__.py
+++ films_api/src/__init__.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_swagger_ui import get_swaggerui_blueprint
+
 import config
 
 app = Flask(__name__)
@@ -22,5 +23,5 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-from . import models, routes
-
+from src import routes
+from src.database import models
